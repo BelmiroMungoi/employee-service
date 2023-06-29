cd38383f3b930392c3b68073ec017320f4b587b5
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-29 08:00:27 +0200

    address entity

---
+++ src/main/java/com/bbm/employeeservice/model/Address.java
@@ -0,0 +1,23 @@
+package com.bbm.employeeservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String houseNumber;
+
+    @Column(nullable = false)
+    private String zipCode;
+
+}
