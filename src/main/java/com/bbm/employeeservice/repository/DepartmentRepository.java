454021ed7e3e294478faa525207b704e1318a59f
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-29 08:02:58 +0200

    repositorys

---
+++ src/main/java/com/bbm/employeeservice/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.bbm.employeeservice.repository;
+
+import com.bbm.employeeservice.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
