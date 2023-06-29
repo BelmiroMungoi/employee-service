b12623f73c495a8c2345a63e233d750fb0cc7cea
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-29 18:50:53 +0200

    adding a dto class

---
+++ src/main/java/com/bbm/employeeservice/model/dto/EmployeeRequest.java
@@ -0,0 +1,23 @@
+package com.bbm.employeeservice.model.dto;
+
+import com.bbm.employeeservice.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeRequest {
+
+    private String employeeIdentifier;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private LocalDate birthdate;
+    private Role role;
+}
---
+++ src/main/java/com/bbm/employeeservice/model/dto/EmployeeResponse.java
@@ -0,0 +1,23 @@
+package com.bbm.employeeservice.model.dto;
+
+import com.bbm.employeeservice.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeResponse {
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private LocalDate birthdate;
+    private Role role;
+}
