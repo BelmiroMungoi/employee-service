5aba1b1e8d47ab1b7c6cf085cb23009b3d10c9f0
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-29 18:49:55 +0200

    adding a criteria search

---
+++ src/main/java/com/bbm/employeeservice/repository/EmployeeSearchDao.java
@@ -0,0 +1,79 @@
+package com.bbm.employeeservice.repository;
+
+import com.bbm.employeeservice.model.Employee;
+import com.bbm.employeeservice.model.dto.SearchRequest;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class EmployeeSearchDao {
+
+    private final EntityManager entityManager;
+
+    public List<Employee> findAllBySimpleQuery(String firstname,
+                                               String lastname,
+                                               String email) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+
+        // SELECT * FROM employee
+        Root<Employee> root = criteriaQuery.from(Employee.class);
+
+        // prepare WHERE clause
+        Predicate firstnamePredicate = criteriaBuilder
+                .like(root.get("firstname"), "%" + firstname + "%");
+        Predicate lastnamePredicate = criteriaBuilder
+                .like(root.get("lastname"), "%" + lastname + "%");
+        Predicate emailPredicate = criteriaBuilder
+                .like(root.get("email"), "%" + email + "%");
+        Predicate firstnameorLastnamePredicate = criteriaBuilder.or(
+                firstnamePredicate, lastnamePredicate, emailPredicate);
+
+        // query => SELECT * FROM employee WHERE firstname like '%an%' OR lastname like '%mk%'
+        // AND email like '%ema%'
+        criteriaQuery.where(criteriaBuilder.and(firstnameorLastnamePredicate, emailPredicate));
+        TypedQuery<Employee> query = entityManager.createQuery(criteriaQuery);
+        return query.getResultList();
+    }
+
+    public List<Employee> findAllByCriteria(SearchRequest request) {
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+        List<Predicate> predicates = new ArrayList<>();
+
+        Root<Employee> root = criteriaQuery.from(Employee.class);
+        if (request.getFirstname() != null) {
+            Predicate firstnamePredicate = criteriaBuilder.like(root
+                    .get("firstname"), "%" + request.getFirstname() + "%");
+            predicates.add(firstnamePredicate);
+        }
+
+        if (request.getLastname() != null) {
+            Predicate lastnamePredicate = criteriaBuilder.like(root
+                    .get("lastname"), "%" + request.getLastname() + "%");
+            predicates.add(lastnamePredicate);
+        }
+
+        if (request.getEmail() != null) {
+            Predicate emailPredicate = criteriaBuilder.like(root
+                    .get("email"),"%" + request.getEmail() + "%");
+            predicates.add(emailPredicate);
+        }
+
+        criteriaQuery.where(criteriaBuilder.or(predicates.toArray(new Predicate[0])));
+
+        TypedQuery<Employee> query = entityManager.createQuery(criteriaQuery);
+        return query.getResultList();
+    }
+}
