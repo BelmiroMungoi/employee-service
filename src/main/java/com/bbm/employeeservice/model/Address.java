79c3033901850cd9ecd8b0e1cfc6224fe9ef9677
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-01 20:57:55 +0200

    adding a lombok annotations

--- src/main/java/com/bbm/employeeservice/model/Address.java
+++ src/main/java/com/bbm/employeeservice/model/Address.java
@@ -1,9 +1,15 @@
 package com.bbm.employeeservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Address {
 
