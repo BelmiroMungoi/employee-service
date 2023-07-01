b29ba7569f4762d0e3defda20df1f5e5109962dd
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-01 21:02:41 +0200

    throwing the exceptions and getting the address

--- src/main/java/com/bbm/employeeservice/service/EmployeeService.java
+++ src/main/java/com/bbm/employeeservice/service/EmployeeService.java
@@ -1,14 +1,17 @@
 package com.bbm.employeeservice.service;
 
+import com.bbm.employeeservice.exception.BusinessException;
+import com.bbm.employeeservice.exception.EntityNotFoundException;
+import com.bbm.employeeservice.model.Address;
 import com.bbm.employeeservice.model.Employee;
 import com.bbm.employeeservice.model.Role;
+import com.bbm.employeeservice.model.dto.AddressResponse;
 import com.bbm.employeeservice.model.dto.EmployeeRequest;
 import com.bbm.employeeservice.model.dto.EmployeeResponse;
 import com.bbm.employeeservice.model.dto.SearchRequest;
 import com.bbm.employeeservice.repository.EmployeeRepository;
 import com.bbm.employeeservice.repository.EmployeeSearchDao;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,19 +19,25 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
     private final EmployeeSearchDao employeeSearch;
+    private final AddressService addressService;
 
     public Employee createEmployee(EmployeeRequest employeeRequest) {
+        if (employeeRepository.existsByEmail(employeeRequest.getEmail())) {
+            throw new BusinessException("Já existe um funcionário registrado com esse Email");
+        }
+        Address saveAddress = addressService.saveAddress(employeeRequest);
+
         Employee employee = Employee.builder()
                 .employeeIdentifier(UUID.randomUUID().toString())
                 .firstname(employeeRequest.getFirstname())
                 .lastname(employeeRequest.getLastname())
                 .email(employeeRequest.getEmail())
                 .birthdate(employeeRequest.getBirthdate())
+                .address(saveAddress)
                 .role(Role.USER)
                 .build();
 
@@ -43,15 +52,18 @@ public class EmployeeService {
 
     public Employee getEmployeeById(Long employeeId) {
         return employeeRepository.findById(employeeId).orElseThrow(() ->
-                new IllegalArgumentException("Funcionário com ID: " + employeeId + " não foi encontrado."));
+                new EntityNotFoundException("Funcionário com ID: " + employeeId + " não foi encontrado."));
     }
 
     public Employee updateEmployee(Long employeeId, EmployeeRequest employeeRequest) {
+        Address saveAddress = addressService.saveAddress(employeeRequest);
+
         Employee employee = getEmployeeById(employeeId);
         employee.setFirstname(employeeRequest.getFirstname());
         employee.setLastname(employeeRequest.getLastname());
         employee.setEmail(employeeRequest.getEmail());
         employee.setBirthdate(employeeRequest.getBirthdate());
+        employee.setAddress(saveAddress);
 
         return employeeRepository.save(employee);
     }
@@ -80,6 +92,11 @@ public class EmployeeService {
                 .lastname(employee.getLastname())
                 .email(employee.getEmail())
                 .birthdate(employee.getBirthdate())
+                .address(AddressResponse.builder()
+                        .houseNumber(employee.getAddress().getHouseNumber())
+                        .street(employee.getAddress().getStreet())
+                        .zipCode(employee.getAddress().getZipCode())
+                        .build())
                 .role(employee.getRole())
                 .build();
     }
