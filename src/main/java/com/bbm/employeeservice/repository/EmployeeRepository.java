85f0ce031fd02617085304d892a8ba47ca25ff22
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-01 21:00:00 +0200

    verifying if employee exists

--- src/main/java/com/bbm/employeeservice/repository/EmployeeRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/EmployeeRepository.java
@@ -4,4 +4,6 @@ import com.bbm.employeeservice.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Boolean existsByEmail(String email);
 }
