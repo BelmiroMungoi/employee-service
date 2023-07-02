a3c3a1d7778c77e384f4b90fab6cefcb7bcec2ae
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-02 17:49:16 +0200

    adding a method to find a department by name

--- src/main/java/com/bbm/employeeservice/repository/DepartmentRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/DepartmentRepository.java
@@ -4,4 +4,5 @@ import com.bbm.employeeservice.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department findByName(String name);
 }
