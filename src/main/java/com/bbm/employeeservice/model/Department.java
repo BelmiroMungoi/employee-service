fd4d00b4653494a69158afe826012f3f102270d8
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-02 17:47:58 +0200

    adding some lombok annotations

--- src/main/java/com/bbm/employeeservice/model/Department.java
+++ src/main/java/com/bbm/employeeservice/model/Department.java
@@ -1,11 +1,17 @@
 package com.bbm.employeeservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Department {
 
