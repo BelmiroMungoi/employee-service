59892c45c85134e94db46facbf2a2409b19cda95
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-02 17:56:07 +0200

    updating the address

--- src/main/java/com/bbm/employeeservice/service/AddressService.java
+++ src/main/java/com/bbm/employeeservice/service/AddressService.java
@@ -2,7 +2,6 @@ package com.bbm.employeeservice.service;
 
 import com.bbm.employeeservice.exception.EntityNotFoundException;
 import com.bbm.employeeservice.model.Address;
-import com.bbm.employeeservice.model.dto.AddressResponse;
 import com.bbm.employeeservice.model.dto.EmployeeRequest;
 import com.bbm.employeeservice.repository.AddressRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,17 +24,18 @@ public class AddressService {
         return addressRepository.save(address);
     }
 
-    public AddressResponse getAddress(Long id) {
-        Address address = addressRepository.findById(id).orElseThrow(() ->
+    public Address getAddress(Long id) {
+        return addressRepository.findById(id).orElseThrow(() ->
                 new EntityNotFoundException("Endereco com ID: " + id + "não foi encontrado"));
-        return mapToAddressResponse(address);
     }
 
-    public AddressResponse mapToAddressResponse(Address address) {
-        return AddressResponse.builder()
-                .houseNumber(address.getHouseNumber())
-                .street(address.getStreet())
-                .zipCode(address.getZipCode())
-                .build();
+    public Address updateAddress(Long id, EmployeeRequest request) {
+        Address address = getAddress(id);
+        address.setHouseNumber(request.getHouseNumber());
+        address.setStreet(request.getStreet());
+        address.setZipCode(request.getZipCode());
+
+        return addressRepository.save(address);
     }
+
 }
