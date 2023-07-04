3eb68f22baea7beac9a6452db8dc0ad852dfb06f
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-04 23:15:52 +0200

    adding a get, put and delete mapping endpoints

--- src/main/java/com/bbm/employeeservice/controller/MissionController.java
+++ src/main/java/com/bbm/employeeservice/controller/MissionController.java
@@ -2,14 +2,14 @@ package com.bbm.employeeservice.controller;
 
 import com.bbm.employeeservice.model.dto.AppResponse;
 import com.bbm.employeeservice.model.dto.MissionRequest;
+import com.bbm.employeeservice.model.dto.MissionResponse;
 import com.bbm.employeeservice.service.MissionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/mission")
@@ -22,5 +22,23 @@ public class MissionController {
     public ResponseEntity<AppResponse> createMission(@RequestBody MissionRequest request) {
         var mission = missionService.createMission(request);
         return new ResponseEntity<>(mission, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<MissionResponse>> getAllMission() {
+        List<MissionResponse> missions = missionService.getAllMission();
+        return new ResponseEntity<>(missions, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AppResponse> updateMission(@PathVariable Long id, @RequestBody MissionRequest request) {
+        var mission = missionService.updateMission(id, request);
+        return new ResponseEntity<>(mission, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteMission(@PathVariable Long id) {
+        missionService.deleteMission(id);
+        return new ResponseEntity<>("Missão com o ID: " + id + " foi deletada", HttpStatus.OK);
     }
 }
