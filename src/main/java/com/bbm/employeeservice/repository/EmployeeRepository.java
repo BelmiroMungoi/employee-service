d449b3ca67616dde4f803b5b98ae2043a3f2b031
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-04 16:38:16 +0200

    changing list to set

--- src/main/java/com/bbm/employeeservice/repository/EmployeeRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/EmployeeRepository.java
@@ -3,7 +3,11 @@ package com.bbm.employeeservice.repository;
 import com.bbm.employeeservice.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.Set;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Boolean existsByEmail(String email);
+    Optional<Set<Employee>> findAllByFirstname(String firstname);
 }
