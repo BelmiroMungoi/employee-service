64b84ac75f10db78fe78d1645fd9b439066f22fb
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-07 20:43:42 +0200

    adding some permissions and authorities

--- src/main/java/com/bbm/employeeservice/model/Role.java
+++ src/main/java/com/bbm/employeeservice/model/Role.java
@@ -1,6 +1,40 @@
 package com.bbm.employeeservice.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.bbm.employeeservice.model.Permission.*;
+
+@RequiredArgsConstructor
 public enum Role {
 
-    USER, ADMIN
+    USER(
+            Set.of(
+                    USER_READ
+            )),
+    ADMIN(
+            Set.of(
+                    ADMIN_CREATE,
+                    ADMIN_READ,
+                    ADMIN_UPDATE,
+                    ADMIN_DELETE
+            )
+    );
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" +this.name()));
+        return authorities;
+    }
 }
