d15dd519b55a4b58107238f1b89c614222d3c651
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-07 20:44:31 +0200

    the user repository

---
+++ src/main/java/com/bbm/employeeservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.bbm.employeeservice.repository;
+
+import com.bbm.employeeservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
