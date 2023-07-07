4113ef435c5c180f958a98c456ef5db573500da4
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-07 20:39:41 +0200

    building the token and extract the claims

---
+++ src/main/java/com/bbm/employeeservice/config/JwtTokenService.java
@@ -0,0 +1,67 @@
+package com.bbm.employeeservice.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtTokenService {
+
+    private static final String SECRET_KEY = "83a8130f89cf1932a6c8cda37c8654041e420dbc34d1f8ef6fa40da18ead3c86";
+
+    public String extractUserEmail(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+     }
+
+     private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+     }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+    return generateToken(userDetails, new HashMap<>());
+    }
+
+    public String generateToken(UserDetails userDetails, Map<String, Object> extraClaims) {
+        return buildToken(extraClaims, userDetails);
+    }
+
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 24))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUserEmail(token);
+        return (username.equals(userDetails.getUsername()));
+    }
+}
