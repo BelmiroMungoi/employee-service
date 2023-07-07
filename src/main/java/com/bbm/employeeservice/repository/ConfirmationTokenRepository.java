fbd8da1f054551c151a6e915a57ba0d8feced931
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-07 20:44:17 +0200

    the repository for confirmation token

---
+++ src/main/java/com/bbm/employeeservice/repository/ConfirmationTokenRepository.java
@@ -0,0 +1,11 @@
+package com.bbm.employeeservice.repository;
+
+import com.bbm.employeeservice.model.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
+}
