6a60ec9b9aac90e1cfd9f9f9e0f00ecde1e509cf
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-07 20:42:42 +0200

    the authenticate response to pass the token

---
+++ src/main/java/com/bbm/employeeservice/model/dto/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package com.bbm.employeeservice.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+}
