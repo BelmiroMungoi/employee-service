76fb2e492de5a943cab57ee3ec4f111d9e96d99c
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-07 20:42:58 +0200

    the register request

---
+++ src/main/java/com/bbm/employeeservice/model/dto/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.bbm.employeeservice.model.dto;
+
+import com.bbm.employeeservice.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+
+}
