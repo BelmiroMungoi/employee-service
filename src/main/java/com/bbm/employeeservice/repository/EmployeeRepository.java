d3989680c70a4aba4420bd5d6754a9f25aeb9df3
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-13 20:37:22 +0200

    putting the user authenticated id

--- src/main/java/com/bbm/employeeservice/repository/EmployeeRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/EmployeeRepository.java
@@ -3,11 +3,17 @@ package com.bbm.employeeservice.repository;
 import com.bbm.employeeservice.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Boolean existsByEmail(String email);
-    Optional<Set<Employee>> findAllByFirstname(String firstname);
+    Optional<Set<Employee>> findAllByFirstnameAndUserId(String firstname);
+    Optional<Employee> findByIdAndUserId(Long employeeId, Long userId);
+    List<Employee> findAllByUserId(Long userId);
+    List<Employee> findAllByDepartmentNameAndUserId(String departmentName, Long userId);
+
+
 }
