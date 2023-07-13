4125bf5836d9fc7e54475d9334e8e3cae098fa99
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-13 20:35:19 +0200

    finding all the departments

--- src/main/java/com/bbm/employeeservice/repository/DepartmentRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/DepartmentRepository.java
@@ -3,6 +3,9 @@ package com.bbm.employeeservice.repository;
 import com.bbm.employeeservice.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Department findByName(String name);
+    List<Department> findAllByNameLikeIgnoreCase(String name);
 }
