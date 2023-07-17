9b104f0c5c0c6aee210f8cbdee468b27f6c7d3f7
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-17 23:41:09 +0200

    finding all valid token

--- src/main/java/com/bbm/employeeservice/repository/ConfirmationTokenRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/ConfirmationTokenRepository.java
@@ -2,10 +2,17 @@ package com.bbm.employeeservice.repository;
 
 import com.bbm.employeeservice.model.ConfirmationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
 
     Optional<ConfirmationToken> findByToken(String token);
+    @Query("""
+                select t from ConfirmationToken t inner join User u on t.user.id = u.id
+                where u.id = :userId and (t.expired = false or t.revoked = false)
+            """)
+    List<ConfirmationToken> findAllValidTokenByUser(Long userId);
 }
