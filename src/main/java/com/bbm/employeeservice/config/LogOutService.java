6a4285545cd8e06e0ec1519e34fc5da758b618a0
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-17 23:38:03 +0200

    adding the logout service tp end the session

---
+++ src/main/java/com/bbm/employeeservice/config/LogOutService.java
@@ -0,0 +1,33 @@
+package com.bbm.employeeservice.config;
+
+import com.bbm.employeeservice.repository.ConfirmationTokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogOutService implements LogoutHandler {
+
+    private final ConfirmationTokenRepository tokenRepository;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+        var storedToken = tokenRepository.findByToken(jwt)
+                .orElseThrow(null);
+        if (storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepository.save(storedToken);
+        }
+    }
+}
