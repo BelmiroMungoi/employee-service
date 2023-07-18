08ba8f90682c42461b71a90add733576adad14bb
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-18 22:16:20 +0200

    adding the report download endpoint

--- src/main/java/com/bbm/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/bbm/employeeservice/controller/EmployeeController.java
@@ -6,13 +6,18 @@ import com.bbm.employeeservice.model.dto.EmployeeRequest;
 import com.bbm.employeeservice.model.dto.EmployeeResponse;
 import com.bbm.employeeservice.model.dto.SearchRequest;
 import com.bbm.employeeservice.service.EmployeeService;
+import com.bbm.employeeservice.service.ReportService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -20,6 +25,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EmployeeController {
 
+    private final ReportService reportService;
     private final EmployeeService employeeService;
 
     @PostMapping("/")
@@ -70,6 +76,19 @@ public class EmployeeController {
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable Long id, @AuthenticationPrincipal User authenticatedUser) {
         employeeService.deleteEmployee(id, authenticatedUser.getId());
-        return ResponseEntity.ok("Funcionário com ID: " + id + " foi Deletado com Sucesso");
+        return ResponseEntity.ok("Funcionário com ID: " + id + " foi Eliminado com Sucesso");
+    }
+
+    @GetMapping("/report")
+    public ResponseEntity<String> downloadReport(HttpServletResponse response) throws IOException {
+        byte[] pdf = reportService.generateReport(new HashMap<>());
+       String base64 = "data:application/pdf;base64," + Base64.encodeBase64String(pdf);
+       /* response.setContentLength(pdf.length);
+        response.setContentType("application/octet-stream");
+        String header = "Content-Disposition";
+        String headerValue = String.format("attachment; filename\"%s\"", "Relatório de Funcionários.pdf");
+        response.setHeader(header, headerValue);
+        response.getOutputStream().write(pdf);*/
+        return new ResponseEntity<>(base64, HttpStatus.OK);
     }
 }
