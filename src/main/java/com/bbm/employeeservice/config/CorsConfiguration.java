36e8b53960958edd8cb06883de87ed2f8bf15b83
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-19 22:55:33 +0200

    adding some cors configuration

---
+++ src/main/java/com/bbm/employeeservice/config/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package com.bbm.employeeservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization")
+                .maxAge(3600L);
+
+    }
+}
