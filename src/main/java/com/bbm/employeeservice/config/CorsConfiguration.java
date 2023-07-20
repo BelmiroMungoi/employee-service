d56bf7b1370aabf60ba7cd335ff2750976741511
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-07-20 19:54:12 +0200

    allowing all origin patterns to access the api

--- src/main/java/com/bbm/employeeservice/config/CorsConfiguration.java
+++ src/main/java/com/bbm/employeeservice/config/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
