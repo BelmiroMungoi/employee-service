d871a521858cb6a56c96f904463474079f3c9600
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-04 22:07:10 +0200

    adding a new attributes in the mission request

--- src/main/java/com/bbm/employeeservice/model/dto/MissionRequest.java
+++ src/main/java/com/bbm/employeeservice/model/dto/MissionRequest.java
@@ -1,10 +1,16 @@
 package com.bbm.employeeservice.model.dto;
 
+import com.bbm.employeeservice.model.MissionStatus;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
 
 @Data
 @Builder
@@ -14,6 +20,8 @@ public class MissionRequest {
 
     @NotBlank
     private String missionName;
-    private int missionDuration;
+    @DateTimeFormat(iso = DATE, pattern = "dd/MM/yyyy")
+    private LocalDateTime finishedDate;
+    private MissionStatus missionStatus;
     private String firstname;
 }
