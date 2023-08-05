b8918d53d295de0aa2eff3c410318b055272c6b5
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-05 16:31:21 +0200

    getting all the status and setting the status into response

--- src/main/java/com/bbm/employeeservice/service/MissionService.java
+++ src/main/java/com/bbm/employeeservice/service/MissionService.java
@@ -7,10 +7,13 @@ import com.bbm.employeeservice.model.User;
 import com.bbm.employeeservice.model.dto.AppResponse;
 import com.bbm.employeeservice.model.dto.MissionRequest;
 import com.bbm.employeeservice.model.dto.MissionResponse;
+import com.bbm.employeeservice.model.dto.StatusResponse;
 import com.bbm.employeeservice.repository.MissionRepository;
+import com.bbm.employeeservice.repository.MissionStatusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
@@ -19,12 +22,15 @@ import java.util.Set;
 public class MissionService {
 
     private final MissionRepository missionRepository;
+    private final MissionStatusRepository missionStatusRepository;
 
     public AppResponse createMission(MissionRequest request, Long userId) {
         User user = new User(userId);
+        MissionStatus status = missionStatusRepository.findByStatus(request.getMissionStatus());
         Mission mission = Mission.builder()
                 .missionName(request.getMissionName())
-                .missionStatus(MissionStatus.ABERTO)
+                .missionStatus(status)
+                .startedAt(LocalDateTime.now())
                 .finishedAt(request.getFinishedDate())
                 .employees(null)
                 .user(user)
@@ -44,7 +50,7 @@ public class MissionService {
     }
 
     public Set<Mission> getMissionByName(String mission, Long userId) {
-        return missionRepository.findAllByMissionNameAndUserId(mission, userId).orElseThrow(() ->
+        return missionRepository.findAllByMissionNameContainsIgnoreCaseAndUserId(mission, userId).orElseThrow(() ->
                 new EntityNotFoundException("Missão com  nome: " + mission + "não foi encontrada"));
     }
 
@@ -53,10 +59,16 @@ public class MissionService {
         return mission.stream().map(this::mapToMissionResponse).toList();
     }
 
+    public List<StatusResponse> getAllStatus() {
+        List<MissionStatus> status = missionStatusRepository.findAll();
+        return status.stream().map(this::mapToStatusResponse).toList();
+    }
+
     public AppResponse updateMission(Long id, MissionRequest request, Long userId) {
+        MissionStatus status = missionStatusRepository.findByStatus(request.getMissionStatus());
         Mission mission = getMissionById(id, userId);
         mission.setMissionName(request.getMissionName());
-        mission.setMissionStatus(request.getMissionStatus());
+        mission.setMissionStatus(status);
         mission.setFinishedAt(request.getFinishedDate());
         missionRepository.save(mission);
 
@@ -78,7 +90,15 @@ public class MissionService {
                 .missionName(mission.getMissionName())
                 .startedDate(mission.getStartedAt())
                 .finishedDate(mission.getFinishedAt())
-                .missionStatus(mission.getMissionStatus().getName())
+                .missionStatus(StatusResponse.builder()
+                        .missionStatus(mission.getMissionStatus().getStatus())
+                        .build())
+                .build();
+    }
+
+    private StatusResponse mapToStatusResponse(MissionStatus status) {
+        return StatusResponse.builder()
+                .missionStatus(status.getStatus())
                 .build();
     }
 }
