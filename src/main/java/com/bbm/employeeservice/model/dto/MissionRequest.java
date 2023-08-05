479dae0adee3a207bbf077ba3899af16e6dae7c6
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-05 16:27:42 +0200

    changing some attributes types

--- src/main/java/com/bbm/employeeservice/model/dto/MissionRequest.java
+++ src/main/java/com/bbm/employeeservice/model/dto/MissionRequest.java
@@ -1,16 +1,12 @@
 package com.bbm.employeeservice.model.dto;
 
-import com.bbm.employeeservice.model.MissionStatus;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
-
-import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
+import java.time.LocalDate;
 
 @Data
 @Builder
@@ -20,8 +16,7 @@ public class MissionRequest {
 
     @NotBlank
     private String missionName;
-    @DateTimeFormat(iso = DATE, pattern = "dd/MM/yyyy")
-    private LocalDateTime finishedDate;
-    private MissionStatus missionStatus;
+    private LocalDate finishedDate;
+    private String missionStatus;
     private String firstname;
 }
