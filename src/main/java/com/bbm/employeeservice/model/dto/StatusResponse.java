6d9c8059a522dfd6d25901b2401b07726cbac62f
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-05 16:28:12 +0200

    the status response dto

---
+++ src/main/java/com/bbm/employeeservice/model/dto/StatusResponse.java
@@ -0,0 +1,15 @@
+package com.bbm.employeeservice.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusResponse {
+
+    private String missionStatus;
+}
