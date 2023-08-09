0189ddeb1fcc7ddad97035e38c8a963d7e5381f5
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-09 16:17:18 +0200

    adding pagination and counting all departments

--- src/main/java/com/bbm/employeeservice/repository/DepartmentRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/DepartmentRepository.java
@@ -1,6 +1,8 @@
 package com.bbm.employeeservice.repository;
 
 import com.bbm.employeeservice.model.Department;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,7 +10,9 @@ import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Department findByNameAndUserId(String name, Long userId);
+    Page<Department> findAllByUserId(PageRequest pageRequest, Long userId);
     List<Department> findAllByUserId(Long userId);
-    List<Department> findAllByNameContainsIgnoreCaseAndUserId(String name, Long userId);
+    Page<Department> findAllByNameContainsIgnoreCaseAndUserId(PageRequest pageRequest, String name, Long userId);
     Optional<Department> findByIdAndUserId(Long id, Long userId);
+    Integer countAllByUserId(Long userId);
 }
