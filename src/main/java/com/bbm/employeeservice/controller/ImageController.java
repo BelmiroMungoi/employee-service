2e0d2c3dd5143e92c0cb5d0c6912d8233ec530c4
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-15 23:09:19 +0200

    downloading the image

--- src/main/java/com/bbm/employeeservice/controller/ImageController.java
+++ src/main/java/com/bbm/employeeservice/controller/ImageController.java
@@ -4,11 +4,7 @@ import com.bbm.employeeservice.model.Image;
 import com.bbm.employeeservice.model.User;
 import com.bbm.employeeservice.service.ImageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -33,13 +29,14 @@ public class ImageController {
         return new ResponseEntity<>(imageService.upload(file, authenticatedUser.getId(), employeeId), HttpStatus.OK);
     }
 
-    @GetMapping("/download/{fileName}")
-    public ResponseEntity<Resource> downloadImage(@PathVariable("fileName") String name) {
-        Image image = imageService.download(name);
-        return ResponseEntity.ok()
+    @GetMapping("/download/")
+    public ResponseEntity<Image> downloadImage(@AuthenticationPrincipal User authenticatedUser) {
+        Image image = imageService.download(authenticatedUser.getId());
+        return ResponseEntity.ok(image);
+        /*return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(image.getFileType()))
                 .header(HttpHeaders.CONTENT_DISPOSITION,
                         "attachment; filename=\"" + image.getOriginalFileName() + "\"")
-                .body(new ByteArrayResource(image.getImage()));
+                .body(new ByteArrayResource(image.getImage()));*/
     }
 }
