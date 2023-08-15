2675b1dd86e0eba47447bc0fed58971dc0c5445a
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-15 23:08:48 +0200

    compressing and decompressing the image

--- src/main/java/com/bbm/employeeservice/service/ImageService.java
+++ src/main/java/com/bbm/employeeservice/service/ImageService.java
@@ -12,6 +12,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
 @Service
 @RequiredArgsConstructor
 public class ImageService {
@@ -32,7 +38,7 @@ public class ImageService {
                 Image image = Image.builder()
                         .originalFileName(file.getOriginalFilename())
                         .fileType(file.getContentType())
-                        .image(file.getBytes())
+                        .image(compressBytes(file.getBytes()))
                         .build();
                 if (employee != null) {
                     image.setEmployee(employee);
@@ -40,7 +46,7 @@ public class ImageService {
                     image.setUser(user);
                 }
                 imageRepository.save(image);
-                return "Imagem foi Carregada com Sucesso: " + file.getOriginalFilename();
+                return "Imagem foi Carregada com Sucesso";
             } catch (Exception e) {
                 throw new BusinessException("Não foi possível salvar a Imagem: " + file.getOriginalFilename() +
                         "\nOcorreu um erro interno. Por favor tente novamente. Caso o erro persista contacte o ADMIN!");
@@ -50,9 +56,52 @@ public class ImageService {
     }
 
     @Transactional
-    public Image download(String fileName) {
-        return imageRepository.findByOriginalFileName(fileName).orElseThrow(() ->
+    public Image download(Long userId) {
+        var img = imageRepository.findByUserId(userId).orElseThrow(() ->
                 new EntityNotFoundException("Não foi possível fazer o download da imagem!"));
+        return Image.builder()
+                .id(img.getId())
+                .originalFileName(img.getOriginalFileName())
+                .fileType(img.getFileType())
+                .image(decompressBytes(img.getImage()))
+                .build();
+    }
+
+    public static byte[] compressBytes(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            throw new BusinessException("Erro ao Salvar Imagem");
+        }
+        return outputStream.toByteArray();
+    }
+
+    public static byte[] decompressBytes(byte[] data) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        try {
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            outputStream.close();
+        } catch (IOException | DataFormatException ioe) {
+            System.out.println(ioe.getMessage());
+            throw new BusinessException("Ocorreu um erro interno ao carregar a imagem");
+        }
+        return outputStream.toByteArray();
     }
 
 }
