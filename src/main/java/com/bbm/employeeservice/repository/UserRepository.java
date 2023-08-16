9b6d418f43240805efd34d66a0a63d4d42057e86
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-16 12:50:34 +0200

    counting all enabled user

--- src/main/java/com/bbm/employeeservice/repository/UserRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/UserRepository.java
@@ -2,12 +2,14 @@ package com.bbm.employeeservice.repository;
 
 import com.bbm.employeeservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-
+    @Query(nativeQuery = true, value = "select count(*) from users where is_enabled = true")
+    Integer countAllEnabledUser();
     boolean existsByEmail(String email);
 }
