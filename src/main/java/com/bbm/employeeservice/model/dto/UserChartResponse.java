976db8c16ac4a9299640909d0a10e0d3401d92df
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-17 12:09:39 +0200

    the chart response

---
+++ src/main/java/com/bbm/employeeservice/model/dto/UserChartResponse.java
@@ -0,0 +1,14 @@
+package com.bbm.employeeservice.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class UserChartResponse {
+
+    private String firstname;
+    private String salary;
+}
