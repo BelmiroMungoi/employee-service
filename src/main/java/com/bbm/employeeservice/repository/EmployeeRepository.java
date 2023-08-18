d907d0ae9c6a36461759f7ac7792ccf524aecdc1
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-18 21:02:17 +0200

    finding all employee by mission id

--- src/main/java/com/bbm/employeeservice/repository/EmployeeRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import com.bbm.employeeservice.model.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,10 +12,19 @@ import java.util.Optional;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Boolean existsByEmail(String email);
+
     Optional<Page<Employee>> findAllByFirstnameContainsIgnoreCaseAndUserId(PageRequest pageRequest, String firstname, Long userId);
+
     Optional<Employee> findByIdAndUserId(Long employeeId, Long userId);
+
     Page<Employee> findAllByUserId(PageRequest pageRequest, Long userId);
+
     List<Employee> findAllByDepartmentNameAndUserId(String departmentName, Long userId);
+
+    @Query(nativeQuery = true, value = "SELECT e.* FROM employee AS e JOIN employee_mission AS " +
+            "em ON e.id = em.employee_id WHERE em.mission_id = :missionId")
+    List<Employee> findAllByMissionId(Long missionId);
+
     Integer countAllByUserId(Long userId);
 
 
