2a48c869faa6fe634fb5a65c65450d81d77e658c
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-08-22 19:30:24 +0200

    verifying if department exists by name and user id

--- src/main/java/com/bbm/employeeservice/repository/DepartmentRepository.java
+++ src/main/java/com/bbm/employeeservice/repository/DepartmentRepository.java
@@ -10,9 +10,16 @@ import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Department findByNameAndUserId(String name, Long userId);
+
     Page<Department> findAllByUserId(PageRequest pageRequest, Long userId);
+
     List<Department> findAllByUserId(Long userId);
+
     Page<Department> findAllByNameContainsIgnoreCaseAndUserId(PageRequest pageRequest, String name, Long userId);
+
     Optional<Department> findByIdAndUserId(Long id, Long userId);
+
     Integer countAllByUserId(Long userId);
+
+    boolean existsByNameAndUserId(String name, Long userId);
 }
