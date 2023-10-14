593316f40f66ca61d07edb076ab9e2b37980eaa2
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-14 15:20:38 +0200

    adding the method to change the user password

--- src/main/java/com/bbm/employeeservice/service/UserService.java
+++ src/main/java/com/bbm/employeeservice/service/UserService.java
@@ -1,19 +1,26 @@
 package com.bbm.employeeservice.service;
 
+import com.bbm.employeeservice.exception.BusinessException;
 import com.bbm.employeeservice.exception.EntityNotFoundException;
 import com.bbm.employeeservice.model.User;
 import com.bbm.employeeservice.model.dto.AppResponse;
+import com.bbm.employeeservice.model.dto.ChangePasswordRequest;
 import com.bbm.employeeservice.model.dto.RegisterRequest;
 import com.bbm.employeeservice.model.dto.UserResponse;
 import com.bbm.employeeservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User getUserById(Long userId) {
         return userRepository.findById(userId).orElseThrow(() ->
@@ -31,6 +38,27 @@ public class UserService {
                 .responseCode("200")
                 .responseMessage("Perfil actualizado com sucesso!")
                 .name(request.getFirstname())
+                .build();
+    }
+
+    public AppResponse changePassword(ChangePasswordRequest request, Principal authenticatedUser) {
+        var user = (User) ((UsernamePasswordAuthenticationToken) authenticatedUser).getPrincipal();
+
+        if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+            throw new BusinessException("Palavra-passe Inválida!");
+        }
+
+        if (!request.getNewPassword().equals(request.getConfirmationPassword())) {
+            throw new BusinessException("Palavra-passe não é igual!");
+        }
+
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        userRepository.save(user);
+
+        return AppResponse.builder()
+                .responseCode("200")
+                .responseMessage("Palavra-passe foi actualizada com sucesso!")
+                .name(user.getFirstname())
                 .build();
     }
 
