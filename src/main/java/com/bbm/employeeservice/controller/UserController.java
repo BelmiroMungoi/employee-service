1b24ce3e9b8fdca965e2e2752d52326ad864e40f
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-14 15:24:53 +0200

    adding a end-point to change the password, and validating the data on update

--- src/main/java/com/bbm/employeeservice/controller/UserController.java
+++ src/main/java/com/bbm/employeeservice/controller/UserController.java
@@ -2,13 +2,17 @@ package com.bbm.employeeservice.controller;
 
 import com.bbm.employeeservice.model.User;
 import com.bbm.employeeservice.model.dto.AppResponse;
+import com.bbm.employeeservice.model.dto.ChangePasswordRequest;
 import com.bbm.employeeservice.model.dto.RegisterRequest;
 import com.bbm.employeeservice.model.dto.UserResponse;
 import com.bbm.employeeservice.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -18,7 +22,7 @@ public class UserController {
     private final UserService userService;
 
     @PutMapping("/")
-    public ResponseEntity<AppResponse> updateUser(@RequestBody RegisterRequest request,
+    public ResponseEntity<AppResponse> updateUser(@Valid @RequestBody RegisterRequest request,
                                                   @AuthenticationPrincipal User authenticatedUser) {
         return ResponseEntity.ok(userService.updateUser(request, authenticatedUser.getId()));
     }
@@ -32,5 +36,11 @@ public class UserController {
     public ResponseEntity<UserResponse> getUserById(@AuthenticationPrincipal User authenticateUser) {
         User user = userService.getUserById(authenticateUser.getId());
         return ResponseEntity.ok(userService.maptoUserResponse(user));
+    }
+
+    @PutMapping ("/changePassword")
+    public ResponseEntity<AppResponse> changePassword(@RequestBody ChangePasswordRequest request,
+                                                      Principal authenticatedUser) {
+        return ResponseEntity.ok(userService.changePassword(request, authenticatedUser));
     }
 }
