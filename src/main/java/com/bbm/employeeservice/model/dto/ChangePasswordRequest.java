067f04d165078bb8beda4cf2c11af668e0422046
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-14 15:21:07 +0200

    adding the data request for change password

---
+++ src/main/java/com/bbm/employeeservice/model/dto/ChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package com.bbm.employeeservice.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ChangePasswordRequest {
+
+    private String currentPassword;
+    private String newPassword;
+    private String confirmationPassword;
+}
