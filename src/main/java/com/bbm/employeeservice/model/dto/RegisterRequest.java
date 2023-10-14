819f379c9370c27e604b4ca6f833e8790b64fae2
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-10-14 15:22:40 +0200

    adding some annotations for validation

--- src/main/java/com/bbm/employeeservice/model/dto/RegisterRequest.java
+++ src/main/java/com/bbm/employeeservice/model/dto/RegisterRequest.java
@@ -1,6 +1,9 @@
 package com.bbm.employeeservice.model.dto;
 
 import com.bbm.employeeservice.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +15,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank
+    @Size(min = 3, max = 60)
     private String firstname;
+
+    @NotBlank
+    @Size(min = 3, max = 30)
     private String lastname;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
+    @Size(min = 4, max = 20)
     private String password;
     private Role role;
 
